e609dc6eda52a8c6ef146d5488328f48a2158aff
Mário Silva <mmcs@outlook.pt>
2018-10-11 22:45:57 +0100

    add scatter Unity3D project test example

--- Unity3D/create_plugins.bat
+++ Unity3D/create_plugins.bat
@@ -8,9 +8,9 @@ if exist "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" (
   "%InstallDir%\MSBuild\15.0\Bin\MSBuild.exe" ScatterSharp.sln /t:ScatterSharp /p:Configuration=Release
   cd ..\Unity3D
   mkdir Plugins
-  copy %userprofile%\.nuget\packages\cryptography.ecdsa.secp256k1\1.1.2\lib\netstandard2.0\Cryptography.ECDSA.dll Plugins\
-  copy %userprofile%\.nuget\packages\eos-sharp\1.0.2\lib\netstandard2.0\EosSharp.dll Plugins\
-  copy %userprofile%\.nuget\packages\fastmember\1.4.1\lib\netstandard2.0\FastMember.dll Plugins\
-  copy %userprofile%\.nuget\packages\newtonsoft.json\11.0.2\lib\netstandard2.0\Newtonsoft.Json.dll Plugins\
-  copy ..\ScatterSharp\ScatterSharp\bin\Release\netstandard2.0\ScatterSharp.dll Plugins\
+  copy %userprofile%\.nuget\packages\cryptography.ecdsa.secp256k1\1.1.2\lib\netstandard2.0\Cryptography.ECDSA.dll ScatterSharpTest\Assets\Plugins\
+  copy %userprofile%\.nuget\packages\eos-sharp\1.0.2\lib\netstandard2.0\EosSharp.dll ScatterSharpTest\Assets\Plugins\
+  copy %userprofile%\.nuget\packages\fastmember\1.4.1\lib\netstandard2.0\FastMember.dll ScatterSharpTest\Assets\Plugins\
+  copy %userprofile%\.nuget\packages\newtonsoft.json\11.0.2\lib\netstandard2.0\Newtonsoft.Json.dll ScatterSharpTest\Assets\Plugins\
+  copy ..\ScatterSharp\ScatterSharp\bin\Release\netstandard2.0\ScatterSharp.dll ScatterSharpTest\Assets\Plugins\
 )
